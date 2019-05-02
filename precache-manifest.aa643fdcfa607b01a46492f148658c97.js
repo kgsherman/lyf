364b0180199467557f04d9dfca50d314743e56d5
Kevin Sherman <sherman.kevin.g@gmail.com>
2019-05-02 14:05:58 +0100

    Updates

---
+++ precache-manifest.aa643fdcfa607b01a46492f148658c97.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1700cf5e52144ee99402af00b95cb816",
+    "url": "/lyf/index.html"
+  },
+  {
+    "revision": "6c319fce0dfb99349b52",
+    "url": "/lyf/static/js/2.6a711539.chunk.js"
+  },
+  {
+    "revision": "ce21cd24d120c5c34581",
+    "url": "/lyf/static/js/main.f0e93891.chunk.js"
+  },
+  {
+    "revision": "b8409f4f2fdf88938f36",
+    "url": "/lyf/static/js/runtime~main.6be6d303.js"
+  }
+]);
